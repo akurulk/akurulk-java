08c580089624e384e8e1509ecfb9f238a632493c
Arunoda <arunoda.susiripala@gmail.com>
2012-02-26 01:49:32 +0530

    project skeliton added added UnicodeSinhala Converter

---
+++ src/main/java/com/kodeincloud/akurulk/UnicodeSinhala.java
@@ -0,0 +1,46 @@
+package com.kodeincloud.akurulk;
+
+/**
+ * Defines Sinhala Unicode Charactor Base
+ */
+public class UnicodeSinhala {
+	
+	public final static int
+		INDEPENDENT_VOWEL = 4,
+		CONSONANT = 8,
+		DEPENDENT_VOWEL = 16,
+		PUNCTUATION = 32,
+		SIGN = 64,
+		OTHER = 128;
+	
+	public static int getType(char c) {
+		
+		int charCode = (int) c;
+		
+		if(charCode >= 3482 && charCode <= 3526) {
+
+			return UnicodeSinhala.CONSONANT;
+		} else if(charCode >= 3535 && charCode <= 3571) {
+
+			return UnicodeSinhala.DEPENDENT_VOWEL;
+		} else if(charCode == 3530) {
+
+			return UnicodeSinhala.DEPENDENT_VOWEL;
+		} else if(charCode >= 3461 && charCode <= 3478) {
+
+			return UnicodeSinhala.INDEPENDENT_VOWEL;
+		} else if(charCode >= 3458 && charCode <= 3459) {
+
+			return UnicodeSinhala.SIGN;
+		} else if(charCode >= 3458 && charCode <= 3459) {
+
+			return UnicodeSinhala.SIGN;
+		} else if(charCode == 3572) {
+
+			return UnicodeSinhala.PUNCTUATION;
+		} else {
+
+			return UnicodeSinhala.OTHER;
+		}
+	}
+}
---
+++ src/test/java/com/kodeincloud/akurulk/UnicodeSinhalaTest.java
@@ -0,0 +1,63 @@
+package com.kodeincloud.akurulk;
+
+import junit.framework.TestCase;
+
+public class UnicodeSinhalaTest extends TestCase {
+
+	public void testConsonents() {
+		
+		assertEquals(UnicodeSinhala.CONSONANT, UnicodeSinhala.getType('ක'));
+		assertEquals(UnicodeSinhala.CONSONANT, UnicodeSinhala.getType('ච'));
+		assertEquals(UnicodeSinhala.CONSONANT, UnicodeSinhala.getType('ෆ'));
+	}
+	
+	public void testNotAConsonents() {
+		
+		assertFalse(UnicodeSinhala.CONSONANT == UnicodeSinhala.getType('ා'));
+	}
+	
+	public void testDependantVowel() {
+		
+		assertEquals(UnicodeSinhala.DEPENDENT_VOWEL, UnicodeSinhala.getType('්'));
+		assertEquals(UnicodeSinhala.DEPENDENT_VOWEL, UnicodeSinhala.getType('ා'));
+		assertEquals(UnicodeSinhala.DEPENDENT_VOWEL, UnicodeSinhala.getType('ා'));
+	}
+	
+	public void testNotDependantVowel() {
+		
+		assertFalse(UnicodeSinhala.DEPENDENT_VOWEL == UnicodeSinhala.getType('ෆ'));
+	}
+	
+	public void testIndependantVowel() {
+		
+		assertEquals(UnicodeSinhala.INDEPENDENT_VOWEL, UnicodeSinhala.getType('අ'));
+		assertEquals(UnicodeSinhala.INDEPENDENT_VOWEL, UnicodeSinhala.getType('ඕ'));
+		assertEquals(UnicodeSinhala.INDEPENDENT_VOWEL, UnicodeSinhala.getType('ඖ'));
+	}
+	
+	public void testNotIndependantVowel() {
+		
+		assertFalse(UnicodeSinhala.INDEPENDENT_VOWEL == UnicodeSinhala.getType('ෆ'));
+	}
+	
+	public void testSign() {
+		
+		assertEquals(UnicodeSinhala.SIGN, UnicodeSinhala.getType('ං'));
+		assertEquals(UnicodeSinhala.SIGN, UnicodeSinhala.getType('ඃ'));
+	}
+	
+	public void testNotSign() {
+		
+		assertFalse(UnicodeSinhala.SIGN == UnicodeSinhala.getType('ා'));
+	}
+	
+	public void testPunctuation() {
+		
+		assertEquals(UnicodeSinhala.PUNCTUATION, UnicodeSinhala.getType('෴'));
+	}
+	
+	public void testNotPunctuation() {
+		
+		assertFalse(UnicodeSinhala.PUNCTUATION == UnicodeSinhala.getType('ා'));
+	}
+}
